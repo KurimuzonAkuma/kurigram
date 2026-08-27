f62f2f8a02901309f3e46554e1dd8659e9566f2b
Danipulok <danipulok@gmail.com>
2026-08-27 10:23:57 +0000

    refactor(connection): keep the package `__init__` imports relative

--- pyrogram/connection/__init__.py
+++ pyrogram/connection/__init__.py
@@ -16,8 +16,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from pyrogram.connection.connection import Connection
-from pyrogram.connection.proxy import (
+from .connection import Connection
+from .proxy import (
     HTTPProxy,
     MTProxy,
     Proxy,
